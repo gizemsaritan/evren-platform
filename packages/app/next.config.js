59fcdd63b65e7574ec5b4bbcc8d9bc27447652cb
Pure Mystic Kids <gizemsaritan@gmail.com>
2026-02-02 13:23:05 +0300

    delay web3 initialization until client is mounted

--- packages/app/next.config.js
+++ packages/app/next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  webpack: (config) => {
-    config.externals.push('pino-pretty', 'lokijs', 'encoding')
-    return config
-  },
-  images: {
-    remotePatterns: [{ hostname: '*' }],
-  },
+  turbopack: {},
 }
 
 module.exports = nextConfig
--- packages/app/src/context/Web3.tsx
+++ packages/app/src/context/Web3.tsx
@@ -1,11 +1,36 @@
 'use client'
 
-import { PropsWithChildren } from 'react'
+import { PropsWithChildren, useEffect, useState } from 'react'
+import { WagmiProvider, type Config } from 'wagmi'
+import { cookieToInitialState } from 'wagmi'
+import { WALLETCONNECT_ADAPTER } from '@/utils/web3'
 
 interface Props extends PropsWithChildren {
   cookies?: string | null
 }
 
 export function Web3Provider(props: Props) {
-  return <>{props.children}</>
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) return null
+
+  const initialState = props.cookies
+    ? cookieToInitialState(
+        WALLETCONNECT_ADAPTER.wagmiConfig as Config,
+        props.cookies
+      )
+    : undefined
+
+  return (
+    <WagmiProvider
+      config={WALLETCONNECT_ADAPTER.wagmiConfig as Config}
+      initialState={initialState}
+    >
+      {props.children}
+    </WagmiProvider>
+  )
 }
